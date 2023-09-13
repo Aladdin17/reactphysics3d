2822d7f2d7c205578e23fab1fe3e7f3a0871eeec
Christien Alden <christien.alden@zoho.com>
2023-09-13 11:04:47 +0800

    [Bugfix] crash when setting localToBodyTransform of Collider
    - A static_cast was being used to check whether the body was a rigidbody
      instead of a dynamic_cast which caused the rigidbody methods to be
      called on a collisionbody.

--- src/collision/Collider.cpp
+++ src/collision/Collider.cpp
@@ -112,7 +112,7 @@ void Collider::setLocalToBodyTransform(const Transform& transform) {
     const Transform& bodyTransform = mBody->mWorld.mTransformComponents.getTransform(mBody->getEntity());
     mBody->mWorld.mCollidersComponents.setLocalToWorldTransform(mEntity, bodyTransform * transform);
 
-    RigidBody* rigidBody = static_cast<RigidBody*>(mBody);
+    RigidBody* rigidBody = dynamic_cast<RigidBody*>(mBody);
     if (rigidBody != nullptr) {
         rigidBody->setIsSleeping(false);
     }
@@ -272,4 +272,3 @@ void Collider::setProfiler(Profiler* profiler) {
 }
 
 #endif
-
